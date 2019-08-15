f4d1a9174aa0b5d1bc02d2aff0df71370e15ab46
paocorrales <paobcorrales@gmail.com>
2019-08-15 18:44:01 -0300

    analisis de obs sin presión

--- notas.R
+++ notas.R
@@ -2,3 +2,6 @@ obs[, ratio := obs.guess/max(1.3, min(5.6, rerr, na.rm = TRUE), na.rm = TRUE)] %
   .[usage.flag == 1 & var == "t"] %>% 
   ggplot(aes(ratio)) +
   geom_density()
+
+
+diag <- fread("E1/test/test.csv", na.strings = c("0.100E+11", "-0.100E+06", "-99999.90", "-100000.00"))
