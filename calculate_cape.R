2ae6430e990a8960a754c0b2a83e446bbbd2d0a9
paocorrales <paobcorrales@gmail.com>
2019-11-12 13:50:22 -0300

    Rscrip para calcular CAPE en Cheyenne

---
+++ calculate_cape.R
@@ -0,0 +1,42 @@
+# Calculate CAPE in a latitud band
+
+library(tidyverse)
+library(data.table)
+library(metR)
+library(aiRthermo)
+
+capear <- function(p, t, q) {
+  # browser()
+  cape <- CAPE_CIN(p, t, q)
+  
+  list(cape = cape$cape,
+       cin = cape$cin,
+       outCode = cape$outCode)
+}
+
+path <- "/glade/scratch/jruiz/EXP/E3/ANA/2018112*/analysis.ensmean"
+lat_band <- list(south_north = 105:109)
+
+
+files <- Sys.glob(path)
+
+for (f in 1:length(files)) {
+  
+  date <- lubridate::ymd_hms(str_extract(path, "\\d{14}"))
+  
+  ana <- ReadNetCDF(files[f], vars = c("P", "PB", "T", "QVAPOR"), subset = lat_band) %>%
+    .[, P := P + PB] %>%
+    .[, T := (T + 290)*(P/100000)^(2/7)] %>%
+    .[, date := date] %>% 
+    .[, c("PB") := NULL]
+  
+  cape <- ana[, capear(P, T, QVAPOR), by = .(south_north, west_east, date)]
+  
+  if (f == 1) {
+    out <- cape
+  } else {
+    out <- rbind(out, temp)
+  }
+}
+
+fwrite(out, "/glade/scratch/jruiz/EXP/analisis/cape_cin_ana_E3.csv")
