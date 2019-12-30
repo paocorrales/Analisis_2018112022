37940e2241e450b6a0ab1d2a2ae439320b5667e9
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2019-12-30 11:18:41 -0300

    Parce errtable casi lista

--- parce_errtable.R
+++ parce_errtable.R
@@ -1,12 +1,34 @@
 library(tidyverse)
 library(data.table)
 
-errtable <- read_lines("prepobs_errtable.global")
+string <- read_lines("prepobs_errtable.global")
 
-errtable %>% 
-  str_which("OBSERVATION TYPE")
-
-errtable %>%  
-  str_split(" ") %>% 
-  map(~ str_subset(.x, "^$", negate = TRUE)) %>% 
+split_chunks <- function(string, pattern = "OBSERVATION TYPE", negate = FALSE) {
+  start <- stringr::str_which(string, pattern, negate = negate)
+  end <- c(start-1, length(string))[-1]
   
+  obs_type <- stringr::str_extract(string[start], "\\d+")
+  
+  chunks <- purrr::map(seq_along(start), ~ string[(start[.x]+1):end[.x]])
+  
+  names(chunks) <- obs_type  
+  chunks
+}
+
+string <- split_chunks(errtable, "OBSERVATION TYPE")
+
+chunk <- string[[1]]
+
+parse_chunk <- function(chunk) {
+  str_split(chunk, " ") %>% 
+    map( ~ str_subset(.x, "^$", negate = TRUE) %>% 
+           setNames(c("P", "T", "Q", "U", "V", "PW")) %>%
+           as.numeric()) %>% 
+    do.call(rbind, .) %>% 
+    as.data.frame()
+}
+
+errtable <- map(string, parse_chunk) %>% 
+  rbindlist(idcol = "type") 
+
+str(errtable)
