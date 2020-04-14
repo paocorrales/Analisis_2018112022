3e3d00d9207813e20ae62ebb4b0d2c4038db6acd
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2020-04-14 18:46:40 -0300

    intentando proyectar el goes toma 1000

--- goes_functions.R
+++ goes_functions.R
@@ -2,7 +2,8 @@
 # projection --------------------------------------------------------------
 
 goes_project <- function(lon, lat, inverse = FALSE, round = TRUE) {
-  map_proj <- "+proj=geos +h=35785831.0 +lon_0=-75 +sweep=x +R=6378137"
+  # map_proj <- "+proj=geos +h=35785831.0 +lon_0=-75 +sweep=x +R=6378137"
+  map_proj <- "+proj=geos +a=6378137. +b=6356752.31414 +lon_0=-89.5 +f=.00335281068119356027 +h=35786023. +sweep=x"
   xy <- proj4::project(list(lon, lat), map_proj, inverse = inverse)
   if (round == TRUE) {
     list(x = round(xy$x, -3),
@@ -18,42 +19,42 @@ ncfile <- nc_open("/home/paola.corrales/datosmunin/DA/DA_DATA/GOES16/OR_ABI-L1b-
 
 proj_info <- ncatt_get(ncfile, "goes_imager_projection")
 lon_0 <- proj_info$longitude_of_projection_origin
-h <- proj_info$perspective_point_height 
+h <- proj_info$perspective_point_height + proj_info$semi_major_axis
 sweep <- proj_info$sweep_angle_axis
-R <- proj_info$semi_major_axis
-   
+
 #https://makersportal.com/blog/2018/11/25/goes-r-satellite-latitude-and-longitude-grid-projection-algorithm
 # GOES-R projection info and retrieving relevant constants
-proj_info = g16nc.variables['goes_imager_projection']
-lon_origin = proj_info.longitude_of_projection_origin
-H = proj_info.perspective_point_height+proj_info.semi_major_axis
-r_eq = proj_info.semi_major_axis
-r_pol = proj_info.semi_minor_axis
+
+#proj_info = g16nc.variables['goes_imager_projection']
+#lon_origin = proj_info.longitude_of_projection_origin
+#H = proj_info.perspective_point_height+proj_info.semi_major_axis
+
+r_eq <- proj_info$semi_major_axis
+r_pol <- proj_info$semi_minor_axis
 
 # Data info
-lat_rad_1d = g16nc.variables['x'][:]
-lon_rad_1d = g16nc.variables['y'][:]
+x_atr <- ncatt_get(ncfile, "x")
+x <- ncvar_get(ncfile, "x")*x_atr$scale_factor + x_atr$add_offset
+y_atr <- ncatt_get(ncfile, "y")
+y <- ncvar_get(ncfile, "y")*y_atr$scale_factor + y_atr$add_offset
 
-# close file when finished
-g16nc.close()
-g16nc = None
 
-# create meshgrid filled with radian angles
-lat_rad,lon_rad = np.meshgrid(lat_rad_1d,lon_rad_1d)
+x <- c(x)
+y <- c(y)
 
-# lat/lon calc routine from satellite radian angle vectors
+# lat/lon calc routine from satellite radian angle vector
+lambda_0 <-  (lon_0*pi)/180.0
 
-lambda_0 = (lon_origin*np.pi)/180.0
+a <- sin(x)^2 + cos(x)^2*(cos(y)^2 + (sin(y)*(r_eq/r_pol))^2)
+b <- -2*h*cos(x)*cos(y)
+c <- h^2 - r_eq^2
 
-a_var = np.power(np.sin(lat_rad),2.0) + (np.power(np.cos(lat_rad),2.0)*(np.power(np.cos(lon_rad),2.0)+(((r_eq*r_eq)/(r_pol*r_pol))*np.power(np.sin(lon_rad),2.0))))
-b_var = -2.0*H*np.cos(lat_rad)*np.cos(lon_rad)
-c_var = (H**2.0)-(r_eq**2.0)
+r_s <- (- b - sqrt(b^2 - 4*a*c))/2*a
 
-r_s = (-1.0*b_var - np.sqrt((b_var**2)-(4.0*a_var*c_var)))/(2.0*a_var)
+s_x <- r_s*cos(x)*cos(y)
+s_y <- -r_s*sin(x)
+s_z <- r_s*cos(x)*sin(y)
 
-s_x = r_s*np.cos(lat_rad)*np.cos(lon_rad)
-s_y = - r_s*np.sin(lat_rad)
-s_z = r_s*np.cos(lat_rad)*np.sin(lon_rad)
+lat <- (atan((r_eq/r_pol)^2 * s_z / sqrt((h - s_x)^2 + s_y^2)))*180/pi
+lon <- (lambda_0 - atan(s_y/(h - s_x)))*180/pi
 
-lat = (180.0/np.pi)*(np.arctan(((r_eq*r_eq)/(r_pol*r_pol))*((s_z/np.sqrt(((H-s_x)*(H-s_x))+(s_y*s_y))))))
-lon = (lambda_0 - np.arctan(s_y/(H-s_x)))*(180.0/np.pi)
