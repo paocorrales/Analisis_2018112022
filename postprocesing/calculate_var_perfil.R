e2f46bc97a305d36949d4e1c588df7de4fcab176
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2020-11-27 11:34:40 -0300

    actualizo var_perfil

--- postprocesing/calculate_var_perfil.R
+++ postprocesing/calculate_var_perfil.R
@@ -3,62 +3,83 @@
 library(tidyverse)
 library(data.table)
 library(metR)
+library(lubridate)
 source("../postprocesamiento.R")
 
-path <- "/glade/scratch/jruiz/EXP/E6/ANA/2018112*/analysis.ensmean"
-lat_band <- list(south_north = 40:299)
-lon_band <- list(west_east = 70:180)
+path <- "/home/paola.corrales/datosmunin/EXP/"
 
-files <- Sys.glob(path)
+lat_band <- c(40, 299)
+lon_band <- c(70, 180)
+
+exp <- "E4"
+ini_date <- ymd_hms("20181120180000")
+ciclos <- 67
+
+dates <- seq.POSIXt(ini_date, by = "hour",
+                    length.out = ciclos)
+
 levs <- c(seq(1000, 500, length.out = 20), seq(500, 50, by = -50))
 levs <- unique(levs)
 
-interpol <- function(x, y, z, ...) {
-  IL <- interp::interp(x, y, z, ...)
-  CJ(y = IL$y, x = IL$x)[, z := c(IL$z)] 
-}
 
-for (f in 1:length(files)) {
+
+for (d in 1:length(dates)) {
+  print(paste("Analisis", dates[d]))
   
-  date <- lubridate::ymd_hms(str_extract(files[f], "\\d{14}"))
-  print(paste("Analisis", date))
+  ana_file <- paste0(path, exp, "/ANA/", format(dates[d], "%Y%m%d%H%M%S"), "/analysis.ensmean")
   
-  ana <- ReadNetCDF(files[f], vars = c("P", "PB", "T", "QVAPOR")) %>%
+  ana <- ReadNetCDF(ana_file, vars = c("P", "PB", "T", "QVAPOR")) %>%
     .[, P := P + PB] %>%
     .[, T := (T + 300)*(P/100000)^(2/7)] %>%
-    .[, c("U", "V") := uvmet(files[f])] %>%
+    .[, c("U", "V") := uvmet(ana_file)] %>%
     .[, SPD := sqrt(U^2 + V^2)] %>% 
-    .[, date := date] 
-
-  #ana_sigma <- ana %>% 
-  #  .[, .(T = mean(T, na.rm = TRUE),
-  #        P = mean(P, na.rm = TRUE),
-  #	  QVAPOR = mean(QVAPOR, na.rm = TRUE)), by = .(bottom_top, date)]
-  
-  ana_lev <- ana %>%
-     .[, .(lev = levs, 
-           T = approx(P, T, xout = levs*100)$y, 
-	   QVAPOR = approx(P, QVAPOR, xout = levs*100)$y,
-           U = approx(P, U, xout = levs*100)$y,
-           V = approx(P, V, xout = levs*100)$y,
-           SPD = approx(P, SPD, xout = levs*100)$y), 
-         by = .(date, south_north, west_east)] %>%
+    .[, date := dates[d]] %>% 
+    .[south_north %between% lat_band & west_east %between% lon_band] %>% 
+    .[, .(lev = levs, 
+          T = approx(P, T, xout = levs*100)$y, 
+          QVAPOR = approx(P, QVAPOR, xout = levs*100)$y,
+          U = approx(P, U, xout = levs*100)$y,
+          V = approx(P, V, xout = levs*100)$y,
+          SPD = approx(P, SPD, xout = levs*100)$y), 
+      by = .(date, south_north, west_east)] %>%
      .[, .(T = mean(T, na.rm = TRUE),
            QVAPOR = mean(QVAPOR, na.rm = TRUE),
            U = mean(U, na.rm = TRUE),
            V = mean(V, na.rm = TRUE),
            SPD = mean(SPD, na.rm = TRUE)), by = .(lev, date)]
+  
+  guess_file <- paste0(path, exp, "/GUESS/", format(dates[d], "%Y%m%d%H%M%S"), "/wrfarw.ensmean")
+  
+  guess <- ReadNetCDF(guess_file, vars = c("P", "PB", "T", "QVAPOR")) %>%
+    .[, P := P + PB] %>%
+    .[, T := (T + 300)*(P/100000)^(2/7)] %>%
+    .[, c("U", "V") := uvmet(ana_file)] %>%
+    .[, SPD := sqrt(U^2 + V^2)] %>% 
+    .[, date := dates[d]] %>% 
+    .[south_north %between% lat_band & west_east %between% lon_band] %>% 
+    .[, .(lev = levs, 
+          T = approx(P, T, xout = levs*100)$y, 
+          QVAPOR = approx(P, QVAPOR, xout = levs*100)$y,
+          U = approx(P, U, xout = levs*100)$y,
+          V = approx(P, V, xout = levs*100)$y,
+          SPD = approx(P, SPD, xout = levs*100)$y), 
+      by = .(date, south_north, west_east)] %>%
+    .[, .(T = mean(T, na.rm = TRUE),
+          QVAPOR = mean(QVAPOR, na.rm = TRUE),
+          U = mean(U, na.rm = TRUE),
+          V = mean(V, na.rm = TRUE),
+          SPD = mean(SPD, na.rm = TRUE)), by = .(lev, date)]
  
-  if (f == 1) {
-  #  out_sigma <- ana_sigma
-    out_lev <- ana_lev
+  if (d == 1) {
+    out_guess <- guess
+    out_ana <- ana
   } else {
-  #  out_sigma <- rbind(out_sigma, ana_sigma)
-    out_lev <- rbind(out_lev, ana_lev)
+    out_guess <- rbind(out_guess, guess)
+    out_ana <- rbind(out_ana, ana)
   }
 
 }
 
-#fwrite(out_sigma, "/glade/scratch/jruiz/EXP/analisis/perfil_T_Q_ana_E4.csv")
-fwrite(out_lev, "/glade/scratch/jruiz/EXP/analisis/perfiles_ana_E6.csv")
+fwrite(out_guess, paste0(path, "/derived_data/perfiles_guess_", exp, ".csv"))
+fwrite(out_ana, paste0(path, "/derived_data/perfiles_ana_", exp, ".csv"))
 
