cc8a452b62db255860191bb636ba963568916dd3
cima.uba-paola.corrales <paola.corrales@cima.fcen.uba.ar>
2021-03-28 20:47:44 -0300

    interpolación de GUESS a sondeos

---
+++ postprocesing/run_interp_sondeos_GUESS.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+module load R
+
+FCST='2018112206'	# Fecha de pronóstico con formato YYYYMMDDHH
+EXP='E7'		# Nombre del experimento
+TMEM=60                 # Cantidad de miembros del ensamble
+
+MEM=1
+
+while [ $MEM -le $TMEM ]
+do
+	echo "************** Member #"${MEM}" ***************" 
+
+	Rscript interp_sondeos.R $EXP $(printf '%02d' $((10#$MEM))) $FCST 
+	MEM=$[$MEM+1]
+done
+
